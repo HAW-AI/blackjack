71e13afb900ae747722de875cb9f2d08d434c3bb
Patrick Detlefsen <patrick.detlefsen@gmail.com>
2013-05-28 12:33:32 +0200

    A* comments

--- lib/blackjack/deck.rb
+++ lib/blackjack/deck.rb
@@ -92,11 +92,15 @@ class Deck
   # tutorial from http://www.policyalmanac.org/games/aStarTutorial.htm
   def a_star(start_value, target_value)
     highest_card_value = cards.map(&:value).max
+    # heuristic function. v is the total value of the dealer's hand
     h = lambda { |v| ((target_value - v) / highest_card_value).ceil }
 
+    # a hash showing how often the same card exists in the deck
     card_counts = Hash[cards.group_by { |c| c }.map { |c, cs| [c, cs.length] }]
 
+    # the open list is an array of hashes
     open_list = cards.map do |card|
+      # total dealer hand value plus the current card value
       sum = start_value + card.value
       h_value = h.call(sum)
       counts = Hash.new(0) # card counts along the path
@@ -104,30 +108,42 @@ class Deck
       { parent: nil, card: card, sum: sum, f: h_value + 1, g: 1, h: h_value, counts: counts }
     end
 
+    # list of hand values of already visited hand combinations
     closed_list = []
+    # array of cards that a part of the hand we are lookong for
     path = []
 
+    # loop until we have visited all hand combinations or we have already
+    # generated a winning hand
     until open_list.empty? || !path.empty?
+      # pick record with lowest cost function value
       record = open_list.min_by { |r| r[:f] }
 
+      # remove selected record form open_list
       open_list.delete_at(open_list.index(record))
+      # add total dealer hand value of record to closed list
       closed_list = closed_list + [record[:sum]]
 
       if record[:sum] == target_value
+        # we have reached our goal. now construct a path from the record back to the start
         path = path_from_record(record)
       elsif record[:sum] < 17 # only allowed to take cards while under 17
         cards.select do |card| # only walkable nodes
           is_card_drawable = card_counts[card] > record[:counts][card]
           sum = record[:sum] + card.value
+          # select card if the sum of the dealer's hand plus the drawn card is not a bust
           sum <= 21 && !closed_list.include?(sum) && is_card_drawable
-        end.each do |card|
+        end.each do |card| # cards are now all available for drawing
           sum = record[:sum] + card.value
           counts = record[:counts].clone
           counts[card] += 1
 
+          # check if there is another way to this result which is longer
           existing_record = open_list.find { |r| r[:sum] == sum }
 
+          # past path cost function g
           g_value = record[:g] + 1
+          # future path cost function h
           h_value = h.call(sum)
           f_value = g_value + h_value
 
